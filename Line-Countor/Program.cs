22bbc1e244c529ec0312905dba013c5a3ccac5d4
hesam hosseini <134384375+nuclearHesam@users.noreply.github.com>
2023-12-29 02:25:15 +0330

    Add project files.

---
+++ Line-Countor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string projectPath;
+        projectPath = Console.ReadLine();
+
+        List<string> extentions = new List<string>();
+        string extention = null;
+
+        while (extention != "")
+        {
+            extentions.Add(extention);
+            Console.Write("Please Enter extentions:");
+            extention = Console.ReadLine();
+        }
+
+        int totalLinesOfCode = CountLinesOfCode(projectPath, extentions);
+
+        Console.WriteLine($"Total lines of code files: {totalLinesOfCode}");
+
+        Console.ReadKey();
+    }
+
+    static int CountLinesOfCode(string path, List<string> extentions)
+    {
+        try
+        {
+            int totalLines = 0;
+
+            foreach (var extention in extentions)
+            {
+                var files = Directory.GetFiles(path, "*." + extention,SearchOption.TopDirectoryOnly);
+
+                foreach (var file in files)
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    totalLines += lines.Length;
+                }
+            }
+
+            return totalLines;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return -1;
+        }
+    }
+}
